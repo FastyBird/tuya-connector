21182efcf915b5eec6fc71e33a68bcb6c738f1a5
Adam Kadlec <adam.kadlec@gmail.com>
2022-07-07 22:29:15 +0200

    Fixing QA

--- src/Entities/TuyaConnector.php
+++ src/Entities/TuyaConnector.php
@@ -46,7 +46,7 @@ class TuyaConnector extends DevicesModuleEntities\Connectors\Connector implement
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getSource()
+	public function getSource(): MetadataTypes\ModuleSourceType|MetadataTypes\PluginSourceType|MetadataTypes\ConnectorSourceType
 	{
 		return MetadataTypes\ConnectorSourceType::get(MetadataTypes\ConnectorSourceType::SOURCE_CONNECTOR_TUYA);
 	}
--- src/Entities/TuyaDevice.php
+++ src/Entities/TuyaDevice.php
@@ -46,7 +46,7 @@ class TuyaDevice extends DevicesModuleEntities\Devices\Device implements ITuyaDe
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getSource()
+	public function getSource(): MetadataTypes\ModuleSourceType|MetadataTypes\PluginSourceType|MetadataTypes\ConnectorSourceType
 	{
 		return MetadataTypes\ConnectorSourceType::get(MetadataTypes\ConnectorSourceType::SOURCE_CONNECTOR_TUYA);
 	}
