81551a020a132a5b2fe4858b3ae4e66c7dc1367b
GitHub <action@github.com>
2024-10-19 11:33:51 +0000

    [ns-pannel-connector] Refactoring connector (#318)

--- src/Queue/Consumers/WriteChannelPropertyState.php
+++ src/Queue/Consumers/WriteChannelPropertyState.php
@@ -510,7 +510,7 @@ final class WriteChannelPropertyState implements Queue\Consumer
 						DevicesStates\Property::ACTUAL_VALUE_FIELD => $state->getExpectedValue(),
 						DevicesStates\Property::EXPECTED_VALUE_FIELD => null,
 					]),
-					MetadataTypes\Sources\Connector::SHELLY,
+					MetadataTypes\Sources\Connector::TUYA,
 				));
 
 				$this->logger->debug(
